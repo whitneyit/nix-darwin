5dec2415a952f5debb7f8a2c9130ae4d139a067b
James Whitney <james.a.whitney@gmail.com>
2024-11-19 10:52:40 +1100

    tmp

--- flake.nix
+++ flake.nix
@@ -25,36 +25,127 @@
         (nerdfonts.override { fonts = [ "FiraCode" ]; })
       ];
 
-      system.defaults = {
-        controlcenter.BatteryShowPercentage = true;
-        controlcenter.Bluetooth = true;
-        dock.autohide  = true;
-        dock.mru-spaces = false;
-        dock.persistent-apps = [
-          "/System/Applications/Utilities/Terminal.app"
-          "/System/Applications/System\ Settings.app/"
-        ];
-        finder.AppleShowAllExtensions = true;
-        finder.FXPreferredViewStyle = "clmv";
-        menuExtraClock.FlashDateSeparators = true;
-        menuExtraClock.IsAnalog = false;
-        menuExtraClock.Show24Hour = true;
-        menuExtraClock.ShowAMPM = false;
-        menuExtraClock.ShowDayOfMonth = true;
-        menuExtraClock.ShowDayOfWeek = true;
-        menuExtraClock.ShowDate = 1;
-        menuExtraClock.ShowSeconds = false;
-        NSGlobalDomain.AppleICUForce24HourTime = true;
-        NSGlobalDomain.AppleInterfaceStyle = "Dark";
-        NSGlobalDomain.AppleShowAllFiles = true;
-        NSGlobalDomain.KeyRepeat = 2;
-        screencapture.location = "~/Pictures/Screenshots";
-        WindowManager.EnableStandardClickToShowDesktop = false;
+      system = {
+        defaults = {
+          controlcenter = {
+            BatteryShowPercentage = true;
+            Bluetooth = true;
+          };
+
+          CustomUserPreferences = {
+            ".GlobalPreferences" = {
+              AppleSpacesSwitchOnActivate = true;
+            };
+            "com.apple.AdLib" = {
+              allowApplePersonalizedAdvertising = false;
+            };
+            "com.apple.desktopservices" = {
+              DSDontWriteNetworkStores = true;
+              DSDontWriteUSBStores = true;
+            };
+            "com.apple.finder" = {
+              ShowExternalHardDrivesOnDesktop = true;
+              ShowHardDrivesOnDesktop = true;
+              ShowMountedServersOnDesktop = true;
+              ShowRemovableMediaOnDesktop = true;
+              _FXSortFoldersFirst = true;
+              # When performing a search, search the current folder by default
+              FXDefaultSearchScope = "SCcf";
+            };
+            "com.apple.ImageCapture".disableHotPlug = true;
+            "com.apple.screencapture" = {
+              location = "~/Pictures/Screenshots";
+              type = "png";
+            };
+            "com.apple.screensaver" = {
+              askForPassword = 1;
+              askForPasswordDelay = 0;
+            };
+            "com.apple.spaces" = {
+              "spans-displays" = 0;
+            };
+            "com.apple.WindowManager" = {
+              EnableStandardClickToShowDesktop = 0;
+              StandardHideDesktopIcons = 0;
+              HideDesktop = 0;
+              StageManagerHideWidgets = 0;
+              StandardHideWidgets = 0;
+            };
+            NSGlobalDomain = {
+              WebKitDeveloperExtras = true;
+            };
+          };
+  
+          dock = {
+            autohide  = true;
+            mru-spaces = false;
+            persistent-apps = [
+              "/System/Applications/Utilities/Terminal.app"
+              "/System/Applications/System\ Settings.app/"
+            ];
+            show-recents = false;
+          };
+  
+          finder = {
+            _FXShowPosixPathInTitle = true;
+            AppleShowAllExtensions = true;
+            FXEnableExtensionChangeWarning = false;
+            FXPreferredViewStyle = "clmv";
+            QuitMenuItem = true;
+            ShowPathbar = true;
+            ShowStatusBar = true;
+          };
+  
+          menuExtraClock = {
+            FlashDateSeparators = true;
+            IsAnalog = false;
+            Show24Hour = true;
+            ShowAMPM = false;
+            ShowDayOfMonth = true;
+            ShowDayOfWeek = true;
+            ShowDate = 1;
+            ShowSeconds = false;
+          };
+  
+          NSGlobalDomain = {
+            AppleICUForce24HourTime = true;
+            AppleInterfaceStyle = "Dark";
+            AppleKeyboardUIMode = 3;
+            ApplePressAndHoldEnabled = true;
+            AppleShowAllFiles = true;
+            InitialKeyRepeat = 15; # 225ms
+            KeyRepeat = 2; # 30ms
+            NSAutomaticCapitalizationEnabled = false;
+            NSAutomaticDashSubstitutionEnabled = false;
+            NSAutomaticPeriodSubstitutionEnabled = false;
+            NSAutomaticQuoteSubstitutionEnabled = false;
+            NSAutomaticSpellingCorrectionEnabled = false;
+            NSNavPanelExpandedStateForSaveMode = true;
+            NSNavPanelExpandedStateForSaveMode2 = true;
+          };
+
+          screencapture = {
+            location = "~/Pictures/Screenshots";
+          };
+
+          trackpad = {
+            Clicking = true;
+            TrackpadRightClick = true;
+            TrackpadThreeFingerDrag = true;
+          };
+
+          WindowManager = {
+            EnableStandardClickToShowDesktop = false;
+          };
+        };
+
+        keyboard = {
+          enableKeyMapping = true;
+          remapCapsLockToEscape = true;
+        };
+
       };
-
-      system.keyboard.enableKeyMapping = true;
-      system.keyboard.remapCapsLockToEscape = true;
-
+  
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
       # nix.package = pkgs.nix;
